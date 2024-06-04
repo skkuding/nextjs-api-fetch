aa46bb47c5a6d5fd9633d6b46f9e6d4ca0dd9118
Jaemin Choi <1dotolee@gmail.com>
2024-06-04 12:08:12 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const data = await fetch("https://dev.codedang.com/api/contest");
+  const data = await fetch("https://stage.codedang.com/api/contest");
   const json = JSON.stringify(await data.json());
 
   const data2 = await fetch(
-    "https://dev.codedang.com/api/problem?workbookId=1&take=3"
+    "https://stage.codedang.com/api/problem?workbookId=1&take=3"
   );
 
   const json2 = JSON.stringify(await data2.json());
